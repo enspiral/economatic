77db93a65f9f1afd8a204f8fa4f45c8a2efd0039
Craig Ambrose <craig@craigambrose.com>
2013-05-05 12:05:35 +1200

    Added an API class that can be used to call contexts

--- economatic_core/lib/economatic_core.rb
+++ economatic_core/lib/economatic_core.rb
@@ -1,10 +1,15 @@
 require 'active_record'
 require 'sqlite3'
 require 'active_record/connection_adapters/sqlite3_adapter'
+require 'economatic/api'
 
 module Economatic
   ROOT_PATH = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  def self.init
+    db_params = YAML.load(File.read(Economatic::ROOT_PATH + "/config/database.yml"))['test']
+    ActiveRecord::Base.establish_connection db_params
+  end
 end
 
-db_params = YAML.load(File.read(Economatic::ROOT_PATH + "/config/database.yml"))['test']
-ActiveRecord::Base.establish_connection db_params
+Economatic.init

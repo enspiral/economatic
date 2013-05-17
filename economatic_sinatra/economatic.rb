258afd066b5a57d32088705338056d20c481b6ee
Joshua Vial <joshua@enspiral.com>
2013-05-17 19:13:04 +1200

    listing commands on sinatra index

--- economatic_sinatra/economatic.rb
+++ economatic_sinatra/economatic.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'playhouse/theatre'
+require 'economatic_core'
 
-get '/' do
-  'Hello World'
+theatre = Playhouse::Theatre.new(environment: 'development')
+
+theatre.stage do
+  play = Economatic::API.new
+
+  play.commands.each do |command|
+    puts command.method_name
+    get "/#{command.method_name}" do
+      command.methods.inspect
+    end
+  end
+
+  get '/' do
+    output = ""
+    play.commands.each do |command|
+      output += "<a href='/#{command.method_name}'>#{command.method_name}</a><br/>"
+    end
+    output
+  end
 end

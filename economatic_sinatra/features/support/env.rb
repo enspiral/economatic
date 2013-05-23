06b1258948ca183cd1177b2785aee185e290c3e6
Minh Ha <nhat.minh.ha@gmail.com>
2013-05-23 14:35:25 +1200

    rewriten env.rb to prevent future problem with Rack (stack too deep)

--- economatic_sinatra/features/support/env.rb
+++ economatic_sinatra/features/support/env.rb
@@ -1,12 +1,17 @@
 require File.join(File.expand_path(File.dirname(__FILE__)),'../../economatic')
 
-require "Capybara"
-require "Capybara/cucumber"
+require "capybara"
+require "capybara/cucumber"
 require "rspec"
 
-World do
-  Capybara.app = Sinatra::Application
+Capybara.app = EconomaticWeb
 
+class EconomaticWebWorld
   include Capybara::DSL
+  include RSpec::Expectations
   include RSpec::Matchers
+end
+
+World do
+  EconomaticWebWorld.new
 end
